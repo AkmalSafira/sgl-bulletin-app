90dc36a2108ac0f192001b58f3fb2c5d14a0e984
Akmal Safira <akmalsafira@gmail.com>
2018-10-22 04:02:25 +0000

    aunthenticate user password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181022031343) do
+ActiveRecord::Schema.define(version: 20181022035250) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20181022031343) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
